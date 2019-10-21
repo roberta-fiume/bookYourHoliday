5c8f0893d5350a4707db80605b22c26d1da2533c
Roberta <robertaf898@gmail.com>
2019-10-21 14:02:19 +0100

    Added style structure for destinations component, added carousel to Mexico component and created booking

--- src/views/Destinations.vue
+++ src/views/Destinations.vue
@@ -19,39 +19,41 @@
 
 
         <div class="anim-destin-wrapper">
-                <Animation />
+            <div class="animation">
+                    <Animation />
+            </div>
             <div class="destinations-wrapper">
-                <div>
+                <div class="destination-box" id="dest1">
                     <h1>Mexico</h1>
                     <img :src="images.mexico" width="300px"/>
                     <v-btn>
                         <router-link to="/Mexico">Book</router-link>
                     </v-btn>
                 </div>
-                <div>
+                <div class="destination-box" id="dest2">
                     <h1>Dubai</h1>
-                    <img :src="images.dubai"/>
+                    <img :src="images.dubai" width="300px"/>
                     <v-btn>
                         <router-link to="/Dubai">Book</router-link>
                     </v-btn>
                 </div>
-                <div>
+                <div class="destination-box" id="dest3">
                     <h1>Hawaii</h1>
-                    <img :src="images.hawaii"/>
+                    <img :src="images.hawaii" width="300px"/>
                     <v-btn>
                         <router-link to="/Hawaii">Book</router-link>
                     </v-btn>
                 </div>
-                <div>
+                <div class="destination-box" id="dest4">
                     <h1>Maldives</h1>
-                    <img :src="images.maldives"/>
+                    <img :src="images.maldives"  width="300px"/>
                     <v-btn>
                         <router-link to="/Maldives">Book</router-link>
                     </v-btn>
                 </div>
-                <div>
+                <div class="destination-box" id="dest5">
                     <h1>Sardinia</h1>
-                    <img :src="images.sardinia"/>
+                    <img :src="images.sardinia"  width="300px"/>
                     <v-btn>
                         <router-link to="/Sardinia">Book</router-link>
                     </v-btn>
@@ -91,8 +93,34 @@
 </script>
  
  <style>
+
+    .animation {
+        background: green;
+        flex: 1 1 40%;
+    }
+
+   .anim-destin-wrapper {
+       display: flex;
+       flex-direction: row;
+   }
   
     .destinations-wrapper {
+        display: flex;
+        background: blue;
+        flex-direction: row;
+        flex-wrap: wrap;
+        flex: 1 1 60%;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .destination-box {
+        background: yellow;
+        display:  flex;
         flex: 1 1 50%;
+        flex-direction: column; 
+    }
+     #dest5 {
+         flex: 0 auto;
     }
  </style>
--- src/views/Mexico.vue
+++ src/views/Mexico.vue
@@ -1,19 +1,134 @@
 <template>
-    <div>
-        MEXICO
-    
+<div>
+ <h1> MEXICO</h1>
+    <v-carousel>
+        <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        ></v-carousel-item>
+    </v-carousel>
+
+    <div class="booking-bedroom-size">
+        <span>Bedroom: </span>
+        <v-container fluid>
+            <v-radio-group v-model="radios" :mandatory="false">
+            <v-radio label="Single" value="Single"></v-radio>
+            <v-radio label="Double" value="Double"></v-radio>
+            </v-radio-group>
+            <p> You chose: {{ radios || 'null' }}</p>
+        </v-container>
     </div>
+
+    <div class="booking-num-people">
+        <span>Number of people: </span>
+        <span>{{people}}</span>
+        <v-btn class="butt-increment" @click="increment"> + </v-btn>
+    </div>
+
+    <div class="booking-bedroom-type">
+        <span>Bedroom type: </span>
+        <v-container fluid>
+            <v-radio-group v-model="bedroomType" :mandatory="false">
+            <v-radio label="Standard" value="Standard"></v-radio>
+            <v-radio label="Suite" value="Suite"></v-radio>
+            <v-radio label="Gold Suite" value="Gold Suite"></v-radio>
+            </v-radio-group>
+            <p> You chose: {{ bedroomType || 'null' }}</p>
+        </v-container>
+    </div>
+
+    <v-btn @click="showPrice">Show price</v-btn>
+
+    <div class="booking-price" v-if="priceBox" >
+        The booking price is: <span> {{finalPrice}}</span>
+    </div>
+
+</div>
 </template>
 
 <script>
-export default {
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
 
-    data() {
-        return {
-                images: {
-                mexico: require('@/assets/img/mexico.jpeg')
-            }
-        }
-    }
-}
+        radios: 'Single',
+        people: 0,
+        bedroomType: 'Standard',
+        finalPrice: '',
+        priceBox: false,
+        prices: []
+      }
+    },
+
+
+    created() {
+        this.people = 0;
+    },
+
+   methods: {
+       increment() {
+           this.people++;
+       },
+
+       showPrice() {
+           this.priceBox = !this.priceBox;
+           this.createPrices();
+       },
+
+       createPrices() {
+           let bookingInfo = [];
+           bookingInfo.push(this.radios, this.people, this.bedroomType);
+           this.prices = bookingInfo;
+          this.combinations();
+
+       },
+
+       combinations() {
+         if (this.prices[0] === "Single" && this.prices[1] == 1 && this.prices[2] === "Standard") {
+             this.finalPrice = '£50';
+             console.log("FINAL PRICEEE", this.finalPrice)    
+         } else if (this.prices[0] === "Single" && this.prices[1] == 1 && this.prices[2] === "Suite") {
+             this.finalPrice = "We're sorry, we don't have Suites for single bedrooms";
+        } else if (this.prices[0] === "Single" && this.prices[1] == 2 && this.prices[2] === "Standard") {
+             this.finalPrice = "Error! there cannot be two people in a Single bedroom. Please retry.";
+        }  else if (this.prices[0] === "Single" && this.prices[1] == 1 && this.prices[2] === "Gold Suite") {
+             this.finalPrice = "We're sorry, we don't provide Gold Suites for single bedrooms";
+         } else if (this.prices[0] === "Double" && this.prices[1] == 1 && this.prices[2] === "Standard") {
+             this.finalPrice = '£100';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 1 && this.prices[2] === "Standard") {
+              this.finalPrice = '£100';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 1 && this.prices[2] === "Suite") {
+               this.finalPrice = '£170';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 1 && this.prices[2] === "Gold Suite") {
+              this.finalPrice = '£220';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 2 && this.prices[2] === "Standard") {
+             this.finalPrice = '£200';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 2 && this.prices[2] === "Suite") {
+              this.finalPrice = '£270';
+         } else if (this.prices[0] === "Double" && this.prices[1] == 2 && this.prices[2] === "Gold Suite") {
+               this.finalPrice = '£350';
+         }
+       }
+   }
+
+
+  }
 </script>
+
